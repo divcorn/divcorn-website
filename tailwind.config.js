48079336bd9c8a350ccfbb8093a34f56829d10b8
Vivek <vivekascoder@gmail.com>
2021-01-11 20:07:13 +0530

    base

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+    	'sm': {'max': '640px'}
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

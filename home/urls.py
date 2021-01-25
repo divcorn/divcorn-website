12bdd1ef2b5a734dbf984f0d3c267395aa93d104
Vivek <vivekascoder@gmail.com>
2021-01-25 14:59:43 +0530

    ******

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import homeView
+from .views import homeView, portfolioView
+
 
 urlpatterns = [
     path('', homeView, name="home"),
+    path('portfolio', portfolioView, name="portfolio")
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def homeView(request):
     return render(request, 'home/index.html', {})
+
+
+def portfolioView(request):
+    return render(request, "home/portfolio.html", {})
